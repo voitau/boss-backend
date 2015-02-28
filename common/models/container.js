d863ce7217557e720ade3be3143b5b0af23001e0
Alex Voitau <voitau@gmail.com>
2015-02-28 15:57:56 -0800

    Added S3 integration.

--- common/models/container.js
+++ common/models/container.js
@@ -1,3 +1,8 @@
 module.exports = function(Container) {
+  Container.disableRemoteMethod('getContainer', true);
+  Container.disableRemoteMethod('getContainers', true);
+  Container.disableRemoteMethod('destroyContainer', true);
+  Container.disableRemoteMethod('createContainer', true);
+  Container.disableRemoteMethod('removeFile', true);
 
 };
--- server/datasources.production.js
+++ server/datasources.production.js
@@ -4,5 +4,11 @@ module.exports = {
     name: "bossdb",
     connector: "mongodb",
     url: process.env.BOSSDB_URL
+  },
+  "bossimgs": {
+    connector: 'loopback-component-storage',
+    provider: 'amazon',
+    keyId: process.env.STORAGE_KEY_ID,
+    key: process.env.STORAGE_KEY
   }
 };
