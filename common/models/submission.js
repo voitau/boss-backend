db4ddd7b343727658f74acf39a52e0288270d3c5
Alex Voitau <voitau@gmail.com>
2015-02-28 19:47:05 -0800

    "status" field for Submission model was introduced.

--- common/models/submission.js
+++ common/models/submission.js
@@ -1,3 +1,10 @@
 module.exports = function(Submission) {
-
+  Submission.validatesInclusionOf('status', {in: [
+    'CREATED',
+    'ASSIGNED',
+    'PENDING_ACCEPTANCE',
+    'ACCEPTED',
+    'IN_PROGRESS',
+    'COMPLETED'
+  ]});
 };
