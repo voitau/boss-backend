112a3269aea8b86df7c31171ea110f35f2456dae
Alex Voitau <voitau@gmail.com>
2015-03-01 02:46:08 -0800

    Added Payment model and relation to submission.

--- common/models/container.js
+++ common/models/container.js
@@ -1,8 +1,3 @@
 module.exports = function(Container) {
-  Container.disableRemoteMethod('getContainer', true);
-  Container.disableRemoteMethod('getContainers', true);
-  Container.disableRemoteMethod('destroyContainer', true);
-  Container.disableRemoteMethod('createContainer', true);
-  Container.disableRemoteMethod('removeFile', true);
 
 };
